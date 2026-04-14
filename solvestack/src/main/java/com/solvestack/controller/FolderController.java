d542d3542bff293d59feb2aab7bff2f22e32feb0
Deepika Shwetha S <deepikashwetha.sg@gmail.com>
2026-04-14 12:09:49 +0530

    Update CORS policy to include production URL

--- solvestack/src/main/java/com/solvestack/controller/FolderController.java
+++ solvestack/src/main/java/com/solvestack/controller/FolderController.java
@@ -55,7 +55,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/folders")
-@CrossOrigin(origins = "http://localhost:3000")
+// @CrossOrigin(origins = "http://localhost:3000")
+    @CrossOrigin(origins = {"http://localhost:3000", "https://solvetrack-frontend.vercel.app"})
 public class FolderController {
 
     @Autowired
